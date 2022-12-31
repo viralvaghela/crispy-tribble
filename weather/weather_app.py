4ceb80e9535a65be4a63a01bf7b5db798d4e3f25
viral <viralvaghela7109@gmail.com>
2022-12-31 21:16:21 +0530

    made a weather app

--- weather/weather_app.py
+++ weather/weather_app.py
@@ -1,21 +1,24 @@
-import requests
-city = input("Enter the city name: ")
-KEY = 'YOUR_API_KEY'
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(
-    city, KEY)
-print(url)
-response = requests.get(url)
-data = response.json()
-temp = data['main']['temp']
-weather = data['weather'][0]['main']
-humidity = data['main']['humidity']
-wind_speed = data['wind']['speed']
-latitude = data['coord']['lat']
-longitude = data['coord']['lon']
+# import requests
+# city = input("Enter the city name: ")
+# KEY = 'YOUR_API_KEY'
+# url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(
+#     city, KEY)
+# print(url)
+# response = requests.get(url)
+# data = response.json()
+# temp = data['main']['temp']
+# weather = data['weather'][0]['main']
+# humidity = data['main']['humidity']
+# wind_speed = data['wind']['speed']
+# latitude = data['coord']['lat']
+# longitude = data['coord']['lon']
 
-print("Temperature: {} degree celcius".format(temp))
-print("Weather: {}".format(weather))
-print("Humidity: {}".format(humidity))
-print("Wind Speed: {}".format(wind_speed))
-print("Latitude: {}".format(latitude))
-print("Longitude: {}".format(longitude))
+# print("Temperature: {} degree celcius".format(temp))
+# print("Weather: {}".format(weather))
+# print("Humidity: {}".format(humidity))
+# print("Wind Speed: {}".format(wind_speed))
+# print("Latitude: {}".format(latitude))
+# print("Longitude: {}".format(longitude))
+
+b = "Hello, World!"
+print(b[-0:- 1])
